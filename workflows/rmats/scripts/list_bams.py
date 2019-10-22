34e9bab918b3b17c982982fd360d2e41539554a5
DidrikOlofsson <didrik@Didriks-MacBook-Pro.local>
2019-10-22 11:57:04 +0200

    Added input function

--- workflows/rmats/scripts/list_bams.py
+++ workflows/rmats/scripts/list_bams.py
@@ -2,11 +2,7 @@ from os import path
 from glob import glob
 import rutils.list_tools.lbam as lb
 
-condition = snakemake.params.condition
 output_file = snakemake.output[0]
+bam_files = snakemake.params.bam_files
 
-bams = glob(path.join(snakemake.params.bam_dir, "*", "Aligned.sortedByCoord.out.bam"))
-selected_bams = [x for x in bams if condition in x]
-bam_list = ",".join(selected_bams)
-
-lb.write_list(bam_list, output_file)
+lb.write_list(bam_files, output_file)
