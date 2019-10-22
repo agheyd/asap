7a032e2cce76beaea390b320c5b29d9464f2fb45
DidrikOlofsson <didrik@Didriks-MacBook-Pro.local>
2019-10-22 11:55:58 +0200

    Added input function

--- workflows/miso/scripts/merge_bams.py
+++ workflows/miso/scripts/merge_bams.py
@@ -4,8 +4,8 @@ from snakemake.shell import shell
 log = snakemake.log_fmt_shell(stdout=True, stderr=True)
 
 shell("samtools merge "
-"-b {snakemake.input} "
-"-O BAM "
-"-@ {snakemake.threads} "
 "{snakemake.output} "
+"{snakemake.params.bam_input} "
+"-O BAM "
+"-@ {snakemake.params.ntasks} "
 "{log}")
