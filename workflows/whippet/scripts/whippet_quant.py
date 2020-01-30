12bae54bddd713f71dcfad6b575488fc1b44b00d
Olofsson <didrio87@login.curta.zedat.fu-berlin.de>
2020-01-30 15:01:54 +0100

    Added sam output

--- workflows/whippet/scripts/whippet_quant.py
+++ workflows/whippet/scripts/whippet_quant.py
@@ -1,14 +1,16 @@
 from os import path
 from snakemake.shell import shell
 
-log = snakemake.log_fmt_shell(stdout=True, stderr=True)
-
+#log = snakemake.log_fmt_shell(stdout=True, stderr=True)
+log = snakemake.log
 reads = snakemake.input.reads
-output_dir = snakemake.params.output_dir
+output_dir = snakemake.output.quant.split(".")[0]
+sam_out = snakemake.output.sam
 idx = snakemake.input.idx.split(".")[0]
 
 shell("whippet-quant.jl {reads} "
 "-o {output_dir} "
 "-x {idx} "
 "--biascorrect "
-"{log}")
+"--sam "
+"2> {log} 1> {sam_out}")
